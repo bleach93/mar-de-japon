3749ae8e241357d49b4617d4ff591483ed0cc0a8
bleach93 <earthb0und820127@gmail.com>
2026-05-12 08:40:13 -0700

    added to github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"; //  Se importa astro
+import tailwindcss from "@tailwindcss/vite"; // Se importa Tailwindcss UI/UX
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    site: "https://bleach93.github.io",
+    base: "/mar-de-japon",
+});
